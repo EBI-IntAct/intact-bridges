3d174515f7aafbbdf43da920958c73fa0a9f3e1f
samuel.kerrien <samuel.kerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2010-02-24 12:20:13 +0000

    new method to search for a spreadsheet by name.

--- src/main/java/uk/ac/ebi/intact/google/spreadsheet/SpreadsheetFacade.java
+++ src/main/java/uk/ac/ebi/intact/google/spreadsheet/SpreadsheetFacade.java
@@ -46,6 +46,30 @@ public class SpreadsheetFacade {
         throw new IllegalStateException( "You don't have access to a spreadsheet with key " + key );
     }
 
+    /**
+     * Returns the SpreadsheetEntry for the spreadsheet with the given name.
+     *
+     * @throws java.io.IOException If a network error occurs while trying to communicate with Spreadsheets
+     * @throws com.google.gdata.util.ServiceException
+     *                             If an application-level protocol error occurs while trying to communicate with Spreadsheets
+     */
+    public static SpreadsheetEntry getSpreadsheetWithName( SpreadsheetService service, String name ) throws IOException, ServiceException {
+        URL metafeedUrl = new URL( "http://spreadsheets.google.com/feeds/spreadsheets/private/full" );
+        SpreadsheetFeed spreadsheetFeed = service.getFeed( metafeedUrl, SpreadsheetFeed.class );
+
+        List<SpreadsheetEntry> spreadsheets = spreadsheetFeed.getEntries();
+        for ( SpreadsheetEntry spreadsheet : spreadsheets ) {
+            if ( spreadsheet.getTitle().getPlainText().equals( name ) ) {
+                if( log.isInfoEnabled() ) {
+                    log.info( "Found spreadsheet '" + spreadsheet.getTitle().getPlainText() +
+                              "', Web URL: " + spreadsheet.getSpreadsheetLink().getHref() );
+                }
+                return spreadsheet;
+            }
+        }
+        throw new IllegalStateException( "You don't have access to a spreadsheet with name: " + name );
+    }
+
     public static void deleteAllDataTables(SpreadsheetService service, SpreadsheetEntry spreadsheet) throws IOException, ServiceException {
         FeedURLFactory factory = FeedURLFactory.getDefault();
         URL tableFeedUrl = factory.getTableFeedUrl(spreadsheet.getKey());
