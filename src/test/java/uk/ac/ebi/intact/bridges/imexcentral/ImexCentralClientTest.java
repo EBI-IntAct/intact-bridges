065690fd6ee91f349cfc2ccbbcc992cb31be13a7
samuel.kerrien <samuel.kerrien@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2010-08-24 14:02:51 +0000

    commit my password ... don't bother trying it's already changed.

--- src/test/java/uk/ac/ebi/intact/bridges/imexcentral/ImexCentralClientTest.java
+++ src/test/java/uk/ac/ebi/intact/bridges/imexcentral/ImexCentralClientTest.java
@@ -17,20 +17,4 @@ public class ImexCentralClientTest {
 //        final DefaultImexCentralClient client = new DefaultImexCentralClient();
 //        Assert.assertEquals( "IM-11903", client.getPublicationImexAccession( "17474147" ) );
     }
-
-    @Test
-    @Ignore
-    public void updateAdminGroup() throws Exception {
-
-        String endpoint = DefaultImexCentralClient.IC_TEST;
-//        String endpoint = "http://dip.doe-mbi.ucla.edu:55508/icentral/ws";
-        DefaultImexCentralClient client = new DefaultImexCentralClient( "skerrien", "240178", endpoint );
-
-//        client.createPublicationById( "1" );
-//        client.updatePublicationAdminGroup( "1", Operation.DROP, "INTACT" );
-//        client.updatePublicationAdminGroup( "1", Operation.ADD, "INTACT" );
-
-//        client.updatePublicationAdminUser( "1", Operation.DROP, "INTACT" );
-        client.updatePublicationAdminUser( "1", Operation.ADD, "skerrien" );
-    }
 }
