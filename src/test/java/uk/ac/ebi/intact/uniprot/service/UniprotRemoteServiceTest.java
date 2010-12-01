6647c95e67e723ca4d36b6bce586ae1f671808f7
brunoaranda <brunoaranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2010-12-01 13:50:23 +0000

    Updated assertion

--- src/test/java/uk/ac/ebi/intact/uniprot/service/UniprotRemoteServiceTest.java
+++ src/test/java/uk/ac/ebi/intact/uniprot/service/UniprotRemoteServiceTest.java
@@ -271,7 +271,7 @@ public class UniprotRemoteServiceTest {
         assertEquals( 7, protein.getKeywords().size() );
 
         // cross references
-        assertEquals( 44, protein.getCrossReferences().size() );
+        assertEquals( 45, protein.getCrossReferences().size() );
 
         // splice variants
         assertEquals( 0, protein.getSpliceVariants().size() );
