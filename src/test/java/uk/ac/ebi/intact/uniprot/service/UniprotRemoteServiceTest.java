a18e82dfaed140914911e7f93da5cd709449a83e
brunoaranda <brunoaranda@cf1f37f2-6a01-11de-ba0c-230037762ff9>
2011-05-11 11:44:10 +0000

    Fixed assertion

--- src/test/java/uk/ac/ebi/intact/uniprot/service/UniprotRemoteServiceTest.java
+++ src/test/java/uk/ac/ebi/intact/uniprot/service/UniprotRemoteServiceTest.java
@@ -283,7 +283,7 @@ public class UniprotRemoteServiceTest {
         assertEquals( 7, protein.getKeywords().size() );
 
         // cross references
-        assertEquals( 47, protein.getCrossReferences().size() );
+        assertEquals( 48, protein.getCrossReferences().size() );
 
         // splice variants
         assertEquals( 0, protein.getSpliceVariants().size() );
