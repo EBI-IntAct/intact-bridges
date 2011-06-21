64f338ce696fc66e023acfb6ce97239826ceed1a
marine <marine@ebi.ac.uk>
2011-06-21 07:56:57 +0000

    PicrClientTest : fixed several broken tests

--- src/test/java/uk/ac/ebi/intact/bridges/picr/PicrClientTest.java
+++ src/test/java/uk/ac/ebi/intact/bridges/picr/PicrClientTest.java
@@ -56,7 +56,7 @@ public class PicrClientTest {
             for (String upi : upis){
                 System.out.println(upi);
             }
-            Assert.assertEquals(8, upis.size());
+            Assert.assertEquals(10, upis.size());
         } catch (PicrClientException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
@@ -84,7 +84,7 @@ public class PicrClientTest {
             for (String upi : upis){
                 System.out.println(upi);
             }
-            Assert.assertEquals(8, upis.size());
+            Assert.assertEquals(10, upis.size());
         } catch (PicrClientException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
