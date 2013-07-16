7415c22fc6d3cde1b80212f4d9e82aa6cc65af0b
mdumousseau <mdumousseau@yahoo.com>
2013-07-16 13:17:46 +0000

    Fixed broken tests

--- src/test/java/uk/ac/ebi/intact/bridges/picr/PicrClientTest.java
+++ src/test/java/uk/ac/ebi/intact/bridges/picr/PicrClientTest.java
@@ -56,7 +56,7 @@ public class PicrClientTest {
             for (String upi : upis){
                 System.out.println(upi);
             }
-            Assert.assertEquals(82, upis.size());
+            Assert.assertEquals(121, upis.size());
         } catch (PicrClientException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
@@ -84,7 +84,7 @@ public class PicrClientTest {
             for (String upi : upis){
                 System.out.println(upi);
             }
-            Assert.assertEquals(82, upis.size());
+            Assert.assertEquals(121, upis.size());
         } catch (PicrClientException e) {
             e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
         }
